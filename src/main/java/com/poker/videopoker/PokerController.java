7e182b1ec838136afdc590326c30eb2435baf50e
Luis Calvillo <luis@macbookair.mynetworksettings.com>
2026-03-29 02:40:49 -0400

    fix cors for frontend deploy

--- src/main/java/com/poker/videopoker/PokerController.java
+++ src/main/java/com/poker/videopoker/PokerController.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://videopoker-frontend.onrender.com")
 @RestController
 @RequestMapping("/poker")
 public class PokerController {
